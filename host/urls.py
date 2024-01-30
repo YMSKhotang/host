2c94e0ec03e295071986c954ca1192113e011ab4
sangam3079 <sangam@youmenepal.org>
2024-01-30 11:45:41 +0545

    template added

--- host/urls.py
+++ host/urls.py
@@ -3,5 +3,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.demo, name='demo')
+    path('', views.demo, name='demo'),
+    path('template/', views.template, name='template')
 ]
--- host/views.py
+++ host/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def demo(request):
     return HttpResponse('hello !!')
+
+def template(request):
+    return render (request,'host/index.html')
